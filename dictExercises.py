93f325dd9cc6d2e5438518dd531fbb32c1bb9dca
Webbek <patrykdog160@gmail.com>
2025-11-03 08:57:48 +0100

    Sorting Dictionaries by Values Addded

--- dictExercises.py
+++ dictExercises.py
@@ -1,9 +1,22 @@
+# 2. Add Key to Dictionary
 sampleDict = {
-    0 : 10,
-    1 : 20,
+    "first" : 130,
+    "second" : 2,
+    "third" : 24,
+    "fourth" : 0,
+
 }
 
 print(sampleDict)
-sampleDict[3] = 30
+# sampleDict[3] = 30
 
 print(sampleDict)
+
+#  Sort Dictionary by Value
+
+val_based = {k: v for k, v in sorted(sampleDict.items(), key=lambda item: item[1])}
+val_basedReversed = {k: v for k, v in sorted(sampleDict.items(), key = lambda item: item[1],reverse = True)}
+
+print(val_based)
+print(f"Descending order {val_basedReversed}")
+print(f"Ascending order {val_based}")
