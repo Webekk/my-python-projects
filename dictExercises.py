ca4022bbf1579afe4cdf75cfd85659f40d964ed8
Webbek <patrykdog160@gmail.com>
2025-11-03 09:05:29 +0100

    Added concatenateing dictionaries with various techniques

--- dictExercises.py
+++ dictExercises.py
@@ -20,3 +20,17 @@ val_basedReversed = {k: v for k, v in sorted(sampleDict.items(), key = lambda it
 print(val_based)
 print(f"Descending order {val_basedReversed}")
 print(f"Ascending order {val_based}")
+
+# Concatenate Dictionaries
+
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+
+
+dicConcat = dic1 | dic2 | dic3 # This creates a new dictionary without modifying the old ones Python 3.9+
+dicConcat2 = {**dic1, **dic2, **dic3} # Dictionary unpacking
+dic1.update(dic2)
+dic1.update(dic3)
+print(dic1)
+# print(dicConcat)
