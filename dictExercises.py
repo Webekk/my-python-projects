34c9395cbe9bf2ddaba17e342dc08f0a694905cc
Webbek <patrykdog160@gmail.com>
2025-11-03 09:22:33 +0100

    Added check for existence of the key in the dictionary, used for loop and made a method

--- dictExercises.py
+++ dictExercises.py
@@ -23,14 +23,33 @@ print(f"Ascending order {val_based}")
 
 # Concatenate Dictionaries
 
-dic1={1:10, 2:20}
+dic1={1:100, 2:20, 3: 10, 4: 20, 5 : 10, 6 : 20, 7: 10, 8 : 20, 9: 10, 10: 10}
 dic2={3:30, 4:40}
 dic3={5:50,6:60}
 
 
 dicConcat = dic1 | dic2 | dic3 # This creates a new dictionary without modifying the old ones Python 3.9+
 dicConcat2 = {**dic1, **dic2, **dic3} # Dictionary unpacking
-dic1.update(dic2)
-dic1.update(dic3)
+# dic1.update(dic2) # Changing a dictionary by using function update()
+# dic1.update(dic3)
 print(dic1)
 # print(dicConcat)
+
+
+# Check if the certain key exist in the dictionary (using a for loop)
+
+for k, v in dic1.items():
+    if k == 10:
+        print(f"The key 10 exists in the dictionary")
+
+
+# Check if the certain key exist in the dictionary (using a function)
+
+def checkNumberInDict(number):
+    # Number as a key and dic1 is my dictionary that was created above
+    if number in dic1:
+        print(f"The key {number} is in the dictionary")
+    else:
+        print(f"The key {number} is not in the dictionary")
+
+checkNumberInDict(10)
