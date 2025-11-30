458b24af9937118768b2439cc7c5613d05cf0565
Webbek <patrykdog160@gmail.com>
2025-11-30 21:37:41 +0100

    Made a simple program using weather api, let's me check the weather forecast for any city i want, this is my first API program, can't wait to find more and have more fun

---
+++ weatherAutomation.py
@@ -0,0 +1,28 @@
+# For this program I used the weatherapi.com, it's really cool and free so you can experiment
+# This is my first API call, things are still a little complicated for me but i'm getting used to it
+import requests
+from dotenv import load_dotenv
+import os
+load_dotenv()
+url = "http://api.weatherapi.com/v1/forecast.json"
+city = input("Enter City: ")
+weather_key = os.getenv("WEATHER_API_KEY")
+params = {
+    "key" : weather_key,
+    "q" : city,
+    "days" : 14,
+    "lang" : "pl",
+    "solar" : "yes"
+}
+
+response = requests.get(url,params=params)
+data = response.json()
+print(f"The 14 day weather forecast for {city}")
+
+for day in data["forecast"]["forecastday"]:
+    print(day["date"], "-", day["day"]["avgtemp_c"], "°C")
+    print(data["current"]["condition"]["text"])
+
+
+
+
