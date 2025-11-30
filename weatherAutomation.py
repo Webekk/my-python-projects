ae4fc3e21557155bcc5fbfc23e31e6b11025c76e
Webbek <patrykdog160@gmail.com>
2025-11-30 21:49:29 +0100

    Learn about how json is returned and how to display the desired output from JSON object, understand a lot more about how to use API

--- weatherAutomation.py
+++ weatherAutomation.py
@@ -4,8 +4,8 @@ import requests
 from dotenv import load_dotenv
 import os
 load_dotenv()
-url = "http://api.weatherapi.com/v1/forecast.json"
-city = input("Enter City: ")
+url = "http://api.weatherapi.com/v1/current.json"
+city = "Cracow"
 weather_key = os.getenv("WEATHER_API_KEY")
 params = {
     "key" : weather_key,
@@ -17,12 +17,14 @@ params = {
 
 response = requests.get(url,params=params)
 data = response.json()
-print(f"The 14 day weather forecast for {city}")
+#print(f"The 14 day weather forecast for {city}")
 
-for day in data["forecast"]["forecastday"]:
-    print(day["date"], "-", day["day"]["avgtemp_c"], "°C")
-    print(data["current"]["condition"]["text"])
+# for day in data["forecast"]["forecastday"]:
+#     print(day["date"], "-", day["day"]["avgtemp_c"], "°C")
+#     print(data["current"]["condition"]["text"])
 
-
-
-
+#Showing the weather for the current city
+print(data)
+print(f"The weather forecast for {city}")
+print(f"Current time in {city}: {data["location"]["localtime"]}")
+print(f"Current temperature in {city}: {data["current"]["temp_c"]}°C")
