ff6b0a365c72fd0692852ac252182e0eef82d185
Webbek <patrykdog160@gmail.com>
2025-12-05 10:08:16 +0100

    My first emails send using python, it was a cool experience of connecting to SMTP server, there's so much you can do with that and so many mails you can send quickly, amazing. And also there is so many modules that i'm gonna use, can't wait to automate that

--- email_send.py
+++ email_send.py
@@ -1,34 +1,22 @@
-import os
-
-import requests
 import smtplib, ssl
-
 from dotenv import load_dotenv
+from email.message import EmailMessage
+import os
+load_dotenv()
 
-# Replace with your email provider's SMTP server and your credentials
 smtp_server = "smtp.gmail.com"
-smtp_port = 587 # Or the appropriate port for your provider
+smtp_port = 465
 sender_email = os.getenv("GMAIL")
-sender_password = os.getenv("SMTP_PASSWORD")
+receiver_email = os.getenv("RECEIVER_EMAIL")
+password = os.getenv("SMTP_PASSWORD")
+msg = EmailMessage() #Create an instance of EmailMessage
+msg["Subject"] = "Subject"
+msg["From"] = sender_email
+msg["To"] = receiver_email
 
-receiver_email = os.getenv("GMAIL")
-message = f"""\
-Subject: Hello from Python
-
-This is a test email sent from Python."""
-
-try:
-    server = smtplib.SMTP(smtp_server, smtp_port)
-    server.starttls() # Secure the connection
-    server.login(sender_email, sender_password)
-    server.sendmail(sender_email, receiver_email, message)
-    print("Email sent successfully!")
-except Exception as e:
-    print(f"Error sending email: {e}")
-finally:
-    server.quit()
-
-
-
-
-
+msg.set_content("""Message sent by using python""")
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, smtp_port, context=context) as server:
+    server.login(sender_email, password)
+    server.send_message(msg)
+print(f"Email sent to {receiver_email}")
