000f6c91ffcac884fd0faa632833cec2a2f59bc7
Webbek <patrykdog160@gmail.com>
2025-12-06 11:29:35 +0100

    Changed the weather app so it shows me the weather forecast also for today

--- Everyday_Use/weatherAutomation.py
+++ Everyday_Use/weatherAutomation.py
@@ -21,6 +21,7 @@ response = requests.get(url, params=params)
 data = response.json()
 
 print(data)
+today = data["forecast"]["forecastday"][0] # index 0 = today
 
 # --- PODSTAWOWE INFO ---
 print(f"\nPrognoza pogody dla: {city}")
@@ -28,9 +29,17 @@ print(f"Aktualny czas: {data['location']['localtime']}")
 print(f"Aktualna temperatura: {data['current']['temp_c']} °C")
 print(f"Odczuwalna: {data['current']['feelslike_c']} °C")
 print("-" * 40)
+# Today's weather forecast
+print(f"Weather forecast for {today["date"]} ")
+for hour in today["hour"]:
+    time = hour["time"]
+    temp = hour["temp_c"]
+    condition = hour["condition"]["text"]
+    feels = hour["feelslike_c"]
+    print(f"{time} | Temp: {temp}°C | Odczuwalna: {feels}°C | {condition}")
 
 # --- JUTRZEJSZA PROGNOZA GODZINOWA ---
-tomorrow = data["forecast"]["forecastday"][1]   # index 1 = jutro
+tomorrow = data["forecast"]["forecastday"][1]   # index 1 = tmrw
 
 print(f"\nPrognoza na jutro: {tomorrow['date']}\n")
 
